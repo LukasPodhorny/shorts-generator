9bf819e39897ef51edd2783127c20dcfbe3ac4ad
LukasPodhorny <lpodhorny075@gmail.com>
2026-04-01 12:01:53 +0200

    preview url

--- src/aishorts/modules/video_edit/video_edit.py
+++ src/aishorts/modules/video_edit/video_edit.py
@@ -120,6 +120,7 @@ class TemplateConfig(BaseModel):
 class VideoTemplate(BaseModel):
     edit_template: str
     template_config: TemplateConfig
+    preview_url: str | None = None
 
 
 class AssSubtitles:
